7ae4f6cb2e65db5d38dd6a0d2580f0fdea5b7b38
blite <442226+blite@users.noreply.github.com>
2022-07-23 06:27:05 -0700

    add replicate to edit product

--- app/Filament/Resources/Shop/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/Shop/ProductResource/Pages/EditProduct.php
@@ -3,9 +3,19 @@
 namespace App\Filament\Resources\Shop\ProductResource\Pages;
 
 use App\Filament\Resources\Shop\ProductResource;
+use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditProduct extends EditRecord
 {
     protected static string $resource = ProductResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\ReplicateAction::make()
+                ->requiresConfirmation(),
+            Actions\DeleteAction::make(),
+        ];
+    }
 }
